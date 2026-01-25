dd9a757afb05a74a4d8a7e37b5ffc91eb4e7fa30
Aggy <ag.agrika@gmail.com>
2026-01-25 06:37:19 -0800

    Please enter the commit message for your changes. Lines starting with '#' will be ignored, and an empty message aborts the commit.
    On branch main
    Your branch is up to date with 'origin/main'.
    
    Changes to be committed:
    	new file:   Assets/Materials.meta
    	new file:   Assets/Materials/Mat.mat
    	new file:   Assets/Materials/Mat.mat.meta
    	new file:   Assets/Materials/New Material 1.mat
    	new file:   Assets/Materials/New Material 1.mat.meta
    	new file:   Assets/Materials/New Material.mat
    	new file:   Assets/Materials/New Material.mat.meta
    	new file:   Assets/XRInteractorTriggerForwarder.cs
    	new file:   Assets/XRInteractorTriggerForwarder.cs.meta
    	modified:   Assets/chadoxr.unity
    	new file:   Assets/floormarker 1.mat
    	new file:   Assets/floormarker 1.mat.meta
    	new file:   Assets/floormarker.mat
    	new file:   Assets/floormarker.mat.meta
    	new file:   Assets/okokok 1 1 1 1 1 1 1.prefab
    	new file:   Assets/okokok 1 1 1 1 1 1 1.prefab.meta
    	new file:   Assets/okokok 1 1 1 1 1 1.prefab
    	new file:   Assets/okokok 1 1 1 1 1 1.prefab.meta
    	new file:   Assets/okokok 1 1 1 1 1.prefab
    	new file:   Assets/okokok 1 1 1 1 1.prefab.meta

---
+++ Assets/XRInteractorTriggerForwarder.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
+
+public class XRInteractorTriggerForwarder : MonoBehaviour
+{
+    public XRBaseInteractor interactor;
+    void Awake()
+    {
+        if (interactor == null) interactor = GetComponent<XRBaseInteractor>();
+        if (interactor != null)
+        {
+            interactor.selectEntered.AddListener(OnSelectEntered);
+            interactor.selectExited.AddListener(OnSelectExited);
+        }
+    }
+
+    void OnDestroy()
+    {
+        if (interactor != null)
+        {
+            interactor.selectEntered.RemoveListener(OnSelectEntered);
+            interactor.selectExited.RemoveListener(OnSelectExited);
+        }
+    }
+
+    void OnSelectEntered(SelectEnterEventArgs args)
+    {
+        var go = args.interactableObject?.transform?.gameObject;
+        if (go == null) return;
+        // Try to call the same method your trigger-based buttons expect
+        go.SendMessage("OnTriggerEnter", GetInteractorCollider(), SendMessageOptions.DontRequireReceiver);
+    }
+
+    void OnSelectExited(SelectExitEventArgs args)
+    {
+        var go = args.interactableObject?.transform?.gameObject;
+        if (go == null) return;
+        go.SendMessage("OnTriggerExit", GetInteractorCollider(), SendMessageOptions.DontRequireReceiver);
+    }
+
+    Collider GetInteractorCollider()
+    {
+        // return any Collider on the interactor to mimic physics callback param (may be null)
+        return interactor.transform.GetComponentInChildren<Collider>();
+    }
+}
